b1d1c16c8aebf3083c05cd7067ac32297b89104f
Lipu <Lipu@Sachin>
2022-07-19 11:43:53 +0530

    1st commit

---
+++ MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample1Test.java
@@ -0,0 +1,19 @@
+package org.mavenExample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNGExample1Test {
+   @Test
+   public void practice1Test() {
+	   Reporter.log("TYSS 1--> test1",true);
+   }
+   @Test
+   public void practice2Test() {
+	   Reporter.log("TYSS 2--> test1",true);
+   }
+   @Test
+   public void practice3Test() {
+	   Reporter.log("TYSS 3--> test1",true);
+   }
+}
---
+++ MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample2Test.java
@@ -0,0 +1,24 @@
+package org.mavenExample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNGExample2Test {
+	@Test
+	   public void practice1Test() {
+		   String url=System.getProperty("url");
+		   String browser=System.getProperty("browser");
+		   System.out.println(url);
+		   System.out.println(browser);
+		   Reporter.log("TYSS 1--> test2",true);
+		   
+	   }
+	@Test
+	   public void practice2Test() {
+		   Reporter.log("TYSS 2--> test2",true);
+	   }
+	@Test
+	   public void practice3Test() {
+		   Reporter.log("TYSS 3--> test2",true);
+	   }
+}
