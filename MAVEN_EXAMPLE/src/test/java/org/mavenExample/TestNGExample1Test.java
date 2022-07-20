adeb022ef340a48d7543fdf22dfd679e6bd2c3cf
Lipu <Lipu@Sachin>
2022-07-20 15:08:33 +0530

    commit11

--- MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample1Test.java
+++ MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample1Test.java
@@ -8,7 +8,7 @@ public class TestNGExample1Test {
    public void practice1Test() {
 	   Reporter.log("TYSS 1--> test1",true);
 	    Reporter.log("TYSS 1--> test1",true);
-	    System.out.println("hello    hy   ");
+	    System.out.println("    hy   ");
 	   
    }
    @Test
