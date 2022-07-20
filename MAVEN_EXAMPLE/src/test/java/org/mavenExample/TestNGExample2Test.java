b14771c60eb52d211616d7585518d3dbaae0830a
Lipu <Lipu@Sachin>
2022-07-20 13:00:02 +0530

    commit 6

--- MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample2Test.java
+++ MAVEN_EXAMPLE/src/test/java/org/mavenExample/TestNGExample2Test.java
@@ -8,8 +8,8 @@ public class TestNGExample2Test {
 	   public void practice1Test() {
 		   String url=System.getProperty("url");
 		   String browser=System.getProperty("browser");
-		   System.out.println(url);
-		   System.out.println(browser);
+//		   System.out.println(url);
+//		   System.out.println(browser);
 		   Reporter.log("TYSS 1--> test2",true);
 		   
 	   }
